1aedfc17114e041c1d9ff71c0ec4abca832c4400
AnnaBaryshnikova <baryshnikovaa@yahoo.com>
2021-08-17 21:25:54 +0300

    hw4

---
+++ jenkins.groovy
@@ -0,0 +1,28 @@
+def mvn = "/var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/3.6.3/bin/mvn"
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                sh "${mvn} clean compile"
+            }
+        }
+        stage('Run') {
+            steps {
+                sh "${mvn} install -Dcucumber.filter.tags=\"${TAG}\""
+            }
+        }
+        stage('Allure Report Generation') {
+            steps {
+                allure includeProperties: false,
+                        jdk: '',
+                        results: [[] path: 'target/reports/allure-results']
+            }
+        }
+    }
+    post {
+        always {
+            cleanWs notFailBuild: true
+        }
+    }
+}
